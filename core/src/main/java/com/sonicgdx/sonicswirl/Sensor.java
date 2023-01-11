1e44769a6885ee2910319e1538af19ea9a67a854
SonicGDX <114670430+SonicGDX@users.noreply.github.com>
2023-01-11 19:51:40 +0000

    Move methods to main Sensor class and make it not abstracted. This is because it is not versatile to use multiple classes because one sensor process would switch to another e.g. while on a wall

--- core/src/main/java/com/sonicgdx/sonicswirl/Sensor.java
+++ core/src/main/java/com/sonicgdx/sonicswirl/Sensor.java
@@ -1,6 +1,9 @@
 package com.sonicgdx.sonicswirl;
 
-public abstract class Sensor {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
+
+public class Sensor {
     protected boolean isActive;
     protected float xPosition, yPosition;
 
@@ -13,6 +16,94 @@ public abstract class Sensor {
     }
 
     abstract void process();
+
+    public void floorProcess() {
+        if (xPosition < 0 || yPosition < 0) {
+            tile = TileMap.getEmpty(); distance = -50;
+            return;
+        }
+        //TODO prevent catch block in getTile() from being used.
+
+        int tileX = Math.floorMod(MathUtils.round(xPosition), 128) / 16;
+        int chunkX = MathUtils.round(xPosition) / 128;
+
+        int tileY = Math.floorMod(MathUtils.round(yPosition), 128) / 16;
+        int chunkY = MathUtils.round(yPosition) / 128;
+
+        int block = Math.floorMod(MathUtils.round(xPosition),16); //Different behaviour for negative numbers compared to using %. For
+        // example, -129 % 16 would return -1 which would cause an ArrayIndexOutOfBoundsException. Math.floorMod() would return a positive index in these cases.
+
+        byte height = TileMap.getTile(chunkX,chunkY,tileX,tileY).getHeight(block);
+
+        float checkDistance = ((chunkY * 128) + (tileY * 16) + height) - yPosition;
+
+        if (height == 16)
+        {
+            int tempTileY, tempChunkY;
+            // sensor regression, checks one tile above with downwards facing sensors in an attempt to find surface if the height of the array is full
+            if (tileY < 7)
+            {
+                tempChunkY = chunkY;
+                tempTileY = tileY + 1;
+            }
+            else
+            {
+                tempChunkY = chunkY + 1;
+                tempTileY = 0;
+            }
+
+            height = TileMap.getTile(chunkX,tempChunkY,tileX,tempTileY).getHeight(block);
+            if (height > 0) //TODO outline conditions in comment
+            {
+                chunkY = tempChunkY;
+                tileY = tempTileY;
+
+                checkDistance += height;
+            }
+        }
+
+        else if (height == 0)
+        {
+            // sensor extension, checks one tile below with downwards facing sensors in an attempt to find surface
+            if (tileY == 0)
+            {
+                chunkY--;
+                tileY = 7;
+            }
+            else tileY--;
+
+            height = TileMap.getTile(chunkX,chunkY,tileX,tileY).getHeight(block);
+
+            if (height == 0) checkDistance -= 16;
+            else checkDistance -= (16-height);
+        }
+
+        tile = TileMap.getTile(chunkX,chunkY,tileX,tileY); distance = checkDistance;
+    }
+
+    public void wallProcess() {
+        if (xPosition < 0 || yPosition < 0) {
+            tile = TileMap.getEmpty(); distance = -50;
+            return;
+        }
+        //TODO prevent catch block in getTile() from being used.
+
+        int tileX = Math.floorMod(MathUtils.round(xPosition), 128) / 16;
+        int chunkX = MathUtils.round(xPosition) / 128;
+
+        int tileY = Math.floorMod(MathUtils.round(yPosition), 128) / 16;
+        int chunkY = MathUtils.round(yPosition) / 128;
+
+        int block = Math.floorMod(MathUtils.round(yPosition),16); //Different behaviour for negative numbers compared to using %. For
+        // example, -129 % 16 would return -1 which would cause an ArrayIndexOutOfBoundsException. Math.floorMod() would return a positive index in these cases.
+
+        byte width = TileMap.getTile(chunkX,chunkY,tileX,tileY).getWidth(block);
+
+        float checkDistance = ((chunkX * 128) + (tileX * 16) + width) - yPosition;
+
+        tile = TileMap.getTile(chunkX,chunkY,tileX,tileY); distance = checkDistance;
+        Gdx.app.debug("distance",String.valueOf(distance));
+    }
 
     public void setPosition(float x, float y) {
         xPosition = x; yPosition = y;
