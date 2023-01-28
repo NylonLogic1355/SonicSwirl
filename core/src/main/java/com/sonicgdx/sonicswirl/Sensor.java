bc1cf80ef52831c29fd032ef51320b625657b7cd
SonicGDX <114670430+SonicGDX@users.noreply.github.com>
2023-01-28 22:10:21 +0000

    Revert "Attempt to push players out of ground below the floor but it doesn't work as you would expect"
    This reverts commit 0a1b0a86ecf9f44e8c6e093b43f4ec83a528f1d6.

--- core/src/main/java/com/sonicgdx/sonicswirl/Sensor.java
+++ core/src/main/java/com/sonicgdx/sonicswirl/Sensor.java
@@ -152,7 +152,7 @@ public class Sensor {
             else checkDistance += (16-width);
         }
 
-        tile = TileMap.getTile(chunkX,chunkY,tileX,tileY); distance = checkDistance - 1;
+        tile = TileMap.getTile(chunkX,chunkY,tileX,tileY); distance = checkDistance;
         //Gdx.app.debug("distance",String.valueOf(distance));
     }
 
